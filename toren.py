bfbce0798183e408e0b9a0c5789343e13af71bc2
Aleksandr Semenov <iamsav@gmail.com>
2013-08-18 01:33:15 +0400

    Move works, bumped to version 1.0, all I need finished.

--- toren.py
+++ toren.py
@@ -9,8 +9,8 @@
 # requires module 'transmissionrpc'
 
 __author__    		= 'Aleksandr Semenov <iamsav@gmail.com>'
-__version_major__   = 0
-__version_minor__   = 1
+__version_major__   = 1
+__version_minor__   = 0
 __version__   		= '{0}.{1}'.format(__version_major__, __version_minor__)
 __copyright__ 		= 'Copyright (c) 2013 Aleksandr Semenov <iamsav@gmail.com>'
 __license__   		= 'MIT'
@@ -52,24 +52,20 @@ def make_client(cfg):
                                 password=cfg['TRANSMISSION_PASW'])
 
 
-def rename_torrent(client, fromname, toname):
-  got = None
-  
-  #TODO fetch torrents in order one-by-one till needed one
+def find_torrent(client, mask):
   for torrent in client.get_torrents():
-    #TODO option for varios matching styles?
-    if fnmatch(torrent.name, fromname):
-      got = torrent
-      break
+    if fnmatch(torrent.name, mask):
+      return torrent
   
-  if got is None:
-    print('Not found torrent for {}'.format(fromname))
-    return False
+  print('Not found torrent for {}'.format(mask))
+  exit(1)
 
-  #Ok, got good torrent in got
-  client.rename_torrent_path(got.id, torrent.name, toname)
+
+def rename_torrent(client, torrent, toname):
+ 
+  client.rename_torrent_path(torrent.id, torrent.name, toname)
   print('Rename command sent.')
-  torrent = client.get_torrent(got.id)
+  torrent = client.get_torrent(torrent.id)
   print('Now torrent named {}'.format(torrent.name))
   return torrent.name == toname
 
@@ -78,20 +74,24 @@ def list_torrents(client, mask=None):
   for torrent in client.get_torrents():
     if mask is not None and not fnmatch(torrent.name, mask):
       continue
-    print('{0}\t{1}'.format(torrent.id, torrent.name))
+    print('{0:>3}  {1:<50} {2}'.format(torrent.id, torrent.name, torrent.downloadDir))
 
 
-def move_torrent(client, filemask, path):
-  #TODO make move
-  raise NotImplemented
+def move_torrent(client, torrent, path):
+  client.move_torrent_data(torrent.id, path)
+  print('Move command sent.')
+  torrent = client.get_torrent(torrent.id)
+  print('Now torrent placed {}'.format(torrent.downloadDir))
+  return torrent.downloadDir == path
  
 
 def argparser():
-  parser = argparse.ArgumentParser(description='Transmission torrent renamer.')
+  parser = argparse.ArgumentParser(prog='Toren', description='Transmission torrent renamer.')
   parser.add_argument('filemask', help='Glob mask defines torrent to rename.', nargs='?', default=None)
   parser.add_argument('newname', help='New name for torrent.', nargs='?', default=None)
   parser.add_argument('-m', '--move', help='Move torrent data to specified path.')
   parser.add_argument('-u', '--url', help='Where to find transmission instance, ex user:password@host:port.')
+  parser.add_argument('-V', '--version', action='version', version='%(prog)s {}'.format(__version__))
 
   return parser.parse_args()
 
@@ -125,15 +125,19 @@ if __name__ == '__main__':
     list_torrents(client)
     exit(0)
 
+  torrent = None
   if args.move:
-    move_torrent(client, args.filemask, args.move)
+    torrent = find_torrent(client, args.filemask)
+    move_torrent(client, torrent, args.move)
 
   if not args.newname:
     if not args.move:
       list_torrents(client, args.filemask)
 
   else:
-    if rename_torrent(client, args.filemask, args.newname):
+    if torrent is None:
+      torrent = find_torrent(client, args.filemask)
+    if rename_torrent(client, torrent, args.newname):
       print('Ok.')
     else:
       print('Failed.')
