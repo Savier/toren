773b3ec6a8d59da52df8584a9a7e5c59e209d37a
Aleksandr Semenov <iamsav@gmail.com>
2015-10-13 17:07:43 +0300

    Merge with abandoned version. Noting changes in common.

--- toren.py
+++ toren.py
@@ -22,19 +22,18 @@ Options:
   -V, --version      Prints version info
 '''
 
-__author__    		= 'Aleksandr Semenov <iamsav@gmail.com>'
-__version_major__   = 1
-__version_minor__   = 0
-__version_3__       = 2
-__version__   		= '{0}.{1}.{2}'.format(__version_major__, __version_minor__, __version_3__)
-__copyright__ 		= 'Copyright (c) 2013 Aleksandr Semenov <iamsav@gmail.com>'
-__license__   		= 'MIT'
+__author__ = 'Aleksandr Semenov <iamsav@gmail.com>'
+__version_major__ = 1
+__version_minor__ = 0
+__version_3__ = 2
+__version__ = '{0}.{1}.{2}'.format(__version_major__, __version_minor__, __version_3__)
+__copyright__ = 'Copyright (c) 2013 Aleksandr Semenov <iamsav@gmail.com>'
+__license__ = 'MIT'
 
 
 import sys, os, os.path
 import re
 from fnmatch import fnmatch
-import argparse, textwrap
 from docopt import docopt
 
 import transmissionrpc
@@ -48,19 +47,18 @@ def safeprint(*args, **kwargs):
     print(*[arg.encode(f.encoding, 'replace').decode(f.encoding) for arg in args], **kwargs)
 
 
-
 def load_config():
 
   cfg = {}
 
   try:
-    exec(open(os.path.expanduser('~/toren.config'), encoding='utf-8').read(), cfg) #Little bit insecure
+    exec(open(os.path.expanduser('~/toren.config'), encoding='utf-8').read(), cfg)   # Little bit insecure
   except FileNotFoundError:
     pass
 
   for var in ('HOST', 'PORT', 'USER', 'PASW'):
     fullvar = 'TRANSMISSION_{0}'.format(var)
-    cfg[fullvar] = cfg.get(fullvar) #Fill defaults with None
+    cfg[fullvar] = cfg.get(fullvar)   # Fill defaults with None
 
   return cfg
 
@@ -70,12 +68,11 @@ def make_client(cfg):
     print(cfg)
     print('Not configured. See README.')
     exit(1)
-    
-  return transmissionrpc.Client(cfg['TRANSMISSION_HOST'], 
+
+  return transmissionrpc.Client(cfg['TRANSMISSION_HOST'],
                                 port=cfg['TRANSMISSION_PORT'],
                                 user=cfg['TRANSMISSION_USER'],
                                 password=cfg['TRANSMISSION_PASW'])
-
 
 
 def find_torrents(client, args):
@@ -99,9 +96,9 @@ def find_torrents(client, args):
     if not returned:
         print('None found by mask {}'.format(args['<oldname>']))
 
-        
+
 def rename_torrent(client, torrent, toname):
- 
+
   print('Previous name was "{}".'.format(torrent.name))
   client.rename_torrent_path(torrent.id, torrent.name, toname)
   print('Rename command sent.')
@@ -112,7 +109,7 @@ def rename_torrent(client, torrent, toname):
 
 def move_torrent(client, torrent, path):
   # Undocumented feature - MOVE_DIRS in config is a tuple of predefined directories
-  if path.isdigit() and MOVE_DIRS and len(MOVE_DIRS)>int(path):
+  if path.isdigit() and MOVE_DIRS and len(MOVE_DIRS) > int(path):
     path = MOVE_DIRS[int(path)]
     print('Path expanded to {}'.format(path))
 
@@ -123,7 +120,7 @@ def move_torrent(client, torrent, path):
   print('Now torrent placed {}'.format(torrent.downloadDir))
   print('Ok.' if torrent.downloadDir == path else 'Failed.')
   return torrent.downloadDir == path
- 
+
 
 def parse_url(url):
   URL_RE = re.compile('((?P<user>[^:]+)(:(?P<pasw>.+))?@)?(?P<host>[^@:]+)(:(?P<port>\\d+))?')
@@ -132,37 +129,42 @@ def parse_url(url):
     raise BadUrlSyntax
 
   gr = match.groupdict()
-  #print(gr.get('user'), gr.get('pasw'), gr.get('host'), gr.get('port')) 
+  # print(gr.get('user'), gr.get('pasw'), gr.get('host'), gr.get('port'))
   return gr.get('user'), gr.get('pasw'), gr.get('host'), gr.get('port')
 
 
 if __name__ == '__main__':
-  args = docopt(__doc__, version=__version__)
 
   cfg = load_config()
   if 'LISTING_FORMAT' in cfg:
-    #global LISTING_FORMAT
-    #TODO refactor to object wrapped over client which has config
+    # global LISTING_FORMAT
+    # TODO refactor to object wrapped over client which has config
     LISTING_FORMAT = cfg['LISTING_FORMAT']
 
   if 'MOVE_DIRS' in cfg:
-    #global MOVE_DIRS
-    #TODO refactor to object wrapped over client which has config
+    # global MOVE_DIRS
+    # TODO refactor to object wrapped over client which has config
     MOVE_DIRS = cfg['MOVE_DIRS']
 
+  args = docopt(__doc__, version=__version__ + '''
+  Shortcut dirs:
+{}'''.format('\n'.join('-m{}: {}'.format(i, d) for i, d in enumerate(MOVE_DIRS))))
+
   if args['--url'] is not None:
-    (cfg['TRANSMISSION_USER'], 
-    cfg['TRANSMISSION_PASW'], 
-    cfg['TRANSMISSION_HOST'],
-    cfg['TRANSMISSION_PORT'] ) = parse_url(args['--url'])
+    (cfg['TRANSMISSION_USER'],
+     cfg['TRANSMISSION_PASW'],
+     cfg['TRANSMISSION_HOST'],
+     cfg['TRANSMISSION_PORT']) = parse_url(args['--url'])
+
+  #print(args)
 
   client = make_client(cfg)
-  
+
   for torrent in find_torrents(client, args):
     if args['--move'] is not None:
       move_torrent(client, torrent, args['--move'])
-    
-    if args['<newname>'] is None: 
+
+    if args['<newname>'] is None:
       if args['--move'] is None:
         safeprint(LISTING_FORMAT.format(torrent))
     else:
